a368e7eecf1c4e5d07332c4e65359e5d35cdc7e2
ritabc <ritabc@gmail.com>
2018-09-06 16:31:50 -0700

    continue working on seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,14 @@ end
 ## Seed database with users (Pokemon)
 User.destroy_all
 10.times do |index|
-  User.create!(username: Faker::Pokemon.name)
+  User.create!(username: Faker::Pokemon.unique.name)
 end
 
 ## Seed database with sentences (Hipster speak)
-Sentence.destroy_all
-50.times do |index|
-  Sentence.create!(sentence_text: Faker::Hipster.sentence)
-end
+# Sentence.destroy_all
+# 50.times do |index|
+#   Sentence.create!(sentence_text: Faker::Hipster.sentence)
+# end
 
 ## Seed database with images (Glyphicons)
 images = []
@@ -24,16 +24,19 @@ Dir["/Users/rita/Documents/Epicodus/rails/story_time/app/assets/images/glyphs/**
 end
 
 Image.destroy_all
-20.times do |index|
+## Task 1 first, i'm working on seeding the images table
+
+## Task 2
+# 20.times do |index|
   image_number = 1 + rand(800)
-  images.each do |filename|
-    if filename.includes?(image_number)
-      icon_name = filename.split("image_number")[1].split(".")[0].split("-").join
-    end ## for if - get icon_name from number
-    Image.create(src: "/assets/images/glyphs/glyphicons-#{image_number}-#{icon_name}.png"
-    break
+  images.each do |filename| ## is there a better way to do this than looping through all the files and looking for one matching the image_number?
+    if filename.include?(image_number.to_s)
+      byebug
+      icon_name = filename.split(/\-#{Regexp.escape(image_number.to_s)}\-/)[1].split(".")[0].split("-").join
+    end
+    Image.create(src: "/assets/images/glyphs/glyphicons-#{image_number}-#{icon_name}.png")
   end ## ends each loop for each filename
-end ## ends loop for each seed Image
+# end ## ends loop for each seed Image
 
 puts "Created #{Story.count} stories"
 puts "Created #{User.count} users"
